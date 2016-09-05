42268a8516f25cb59e6a62aed87e0bc51e267c03
Patrik Pettersson <pettersson.pa@gmail.com>
2016-09-05 10:28:17 +0200

    Added Vec3 test suite

---
+++ src/test/scala/se/randomserver/math/test/Vec3Suite.scala
@@ -0,0 +1,16 @@
+package se.randomserver.math.test
+
+
+import se.randomserver.math.Vec3
+import se.randomserver.math.test.VecSuiteGenerators._
+
+/**
+  * Created by patrik on 9/5/16.
+  */
+class Vec3IntSuite extends VecSuite[Int, Vec3]
+
+class Vec3FloatSuite extends VecSuite[Float, Vec3]
+
+class Vec3DoubleSuite extends VecSuite[Double, Vec3]
+
+class Vec3LongSuite extends VecSuite[Long, Vec3]
