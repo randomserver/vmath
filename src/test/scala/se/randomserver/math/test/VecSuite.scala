1368ebfabcf0b4ccba88106e3d5e4f7339c9b51e
Patrik Pettersson <pettersson.pa@gmail.com>
2016-09-05 14:03:45 +0200

    updated tests to use assertions

--- src/test/scala/se/randomserver/math/test/VecSuite.scala
+++ src/test/scala/se/randomserver/math/test/VecSuite.scala
@@ -10,18 +10,44 @@ import se.randomserver.math.{Vec, VecIntegral}
 /**
   * Created by patrik on 9/4/16.
   */
-abstract class VecSuite[T: Arbitrary, V[T] <: Vec[T, V]](implicit genVec: Arbitrary[V[T]], num: VecIntegral[T])
+abstract class VecSuite[T: Arbitrary, V[T] <: Vec[T, V]](tolerance: T)(implicit genVec: Arbitrary[V[T]], num: VecIntegral[T])
   extends PropSpec with PropertyChecks {
+
+  import num._
 
   property("associative add") {
     forAll { (a: V[T], b: V[T]) =>
-      a + b == b + a
+      assert((a + b).compare(b + a) < tolerance)
     }
   }
 
-  property("scalar multiplication") {
+  property("scalar distributed") {
     forAll { (a: V[T], b: V[T], s: T) =>
-      (a + b) * s == a * s + b * s
+      assert((a + b) * s == a * s + b * s)
+    }
+  }
+
+  property("scalar distributed2") {
+    forAll { (a: V[T], s1: T, s2: T) =>
+      assert(a * (s1 + s2) == a * s1 + a * s2)
+    }
+  }
+
+  property("scalar associative") {
+    forAll() { (c: T, d: T, v: V[T]) =>
+      assert(v * (c * d) == (v * d) * c)
+    }
+  }
+
+  property("dot associative") {
+    forAll { (u: V[T], v: V[T]) =>
+      assert(v.dot(u) == u.dot(v))
+    }
+  }
+
+  property("dot square abs") {
+    forAll { (u: V[T]) =>
+      assert(u.dot(u) == u.abs * u.abs)
     }
   }
 }
